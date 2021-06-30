a6364e3de97fdebd0460918e1b5802b753863e17
Chase090 <carloyam21@icloud.com>
2021-06-29 20:02:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+let name = `Carl Oyam`
+document.querySelector('main#main').remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = `victory`
+newHeader.innerHTML = `${name} is the champion`
